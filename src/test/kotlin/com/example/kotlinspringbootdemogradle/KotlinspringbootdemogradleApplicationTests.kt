0b2e328485c10e2c9323208a2417ad05235f2ecc
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-05-13 17:53:16 -0400

    Add files via upload

---
+++ src/test/kotlin/com/example/kotlinspringbootdemogradle/KotlinspringbootdemogradleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringbootdemogradle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinspringbootdemogradleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
